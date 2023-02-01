7905b331e93c0151ffce53c111716e0a10e90151
Vu Van Dung <me@joulev.dev>
2023-02-01 09:03:31 +0800

    add babel-plugin-superjson-next example
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from "next/app";
-import "highlight.js/styles/github.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,17 @@
-import ReactMarkdown from "react-markdown";
-import rehypeHighlight from "rehype-highlight";
-
-export default function Page() {
+export default function Page({ date, bigInt }: { date: Date; bigInt: BigInt }) {
   return (
-    <ReactMarkdown
-      rehypePlugins={[[rehypeHighlight, { ignoreMissing: true }]]}
-    >{`
-\`\`\`tsx
-import { AppProps } from "next/app";
-import "highlight.js/styles/github.css";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-\`\`\`
-`}</ReactMarkdown>
+    <div>
+      <div>Date = {date.toISOString()}</div>
+      <div>BigInt = {bigInt.toString()}</div>
+    </div>
   );
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      date: new Date(),
+      bigInt: BigInt(1234567890),
+    },
+  };
 }
