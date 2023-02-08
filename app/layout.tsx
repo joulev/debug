96a05a2c60ad9abb84d07cc7a3ba96b1952d225c
Vu Van Dung <me@joulev.dev>
2023-02-08 13:16:59 +0800

    use a more minimal appDir app as the starting point
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
-import './globals.css'
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
