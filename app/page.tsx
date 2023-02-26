c79e3ab505843436f0cb4df8d75fe57b52a4aeb8
Vu Van Dung <me@joulev.dev>
2023-02-27 01:21:44 +0800

    fetches working fine
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <div>Hello world</div>;
+export default async function TimesDisplay() {
+  const kz_timer = fetch(
+    `https://kztimerglobal.com/api/v2.0/records/top/recent?modes_list_string=kz_timer&place_top_at_least=1&has_teleports=false&stage=0&limit=5&tickrate=128`
+  ).then((r) => r.json());
+  const kz_simple = fetch(
+    `https://kztimerglobal.com/api/v2.0/records/top/recent?modes_list_string=kz_simple&place_top_at_least=1&has_teleports=false&stage=0&limit=5&tickrate=128`
+  ).then((r) => r.json());
+  const kz_vanilla = fetch(
+    `https://kztimerglobal.com/api/v2.0/records/top/recent?modes_list_string=kz_vanilla&place_top_at_least=1&has_teleports=false&stage=0&limit=5&tickrate=128`
+  ).then((r) => r.json());
+
+  const recent_times = await Promise.all([kz_timer, kz_simple, kz_vanilla]);
+  return <pre>{JSON.stringify(recent_times, null, 2)}</pre>;
 }
