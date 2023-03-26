0c33f72b521510f15a72fbc9b4e240707cc6dd95
Vu Van Dung <me@joulev.dev>
2023-03-26 16:42:53 +0800

    did i just make it work? (2)
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
-import { useCallback, useState } from "react";
-import { createEditor, Descendant, Path } from "slate";
-import { Editable, Slate, withReact } from "slate-react";
+import { useCallback, useEffect, useState } from "react";
+import { createEditor, Descendant, Editor, Element, Node, Path } from "slate";
+import {
+  Editable as SlateEditable,
+  Slate,
+  withReact,
+  useSlateSelection,
+  useSlate,
+} from "slate-react";
 import ActiveNode from "~/components/active-node";
-import { ActivePathContext } from "~/components/active-path-context";
+import {
+  ActivePathContext,
+  useActivePath,
+} from "~/components/active-path-context";
 import RenderElement from "~/components/render-element";
 
 const initialValue: Descendant[] = [
@@ -35,16 +44,42 @@ const initialValue: Descendant[] = [
   },
 ];
 
+function Editable() {
+  const editor = useSlate();
+  const selection = useSlateSelection();
+  const { setPath } = useActivePath();
+  const renderElement = useCallback(RenderElement, []);
+  useEffect(() => {
+    if (!selection) return;
+    let _path = selection.anchor.path.slice();
+    while (true) {
+      const node = Node.get(editor, _path);
+      if (Editor.isEditor(node)) {
+        setPath([]);
+        return;
+      }
+      if (!Element.isElement(node)) {
+        _path = Path.parent(_path);
+        continue;
+      }
+      setPath(_path);
+      return;
+    }
+  }, [editor, selection, setPath]);
+  return (
+    <SlateEditable renderElement={renderElement} onClick={() => setPath([])} />
+  );
+}
+
 export default function Page() {
   const [editor] = useState(() => withReact(createEditor()));
   const [value, setValue] = useState(initialValue);
   const [path, setPath] = useState<Path>([]);
-  const renderElement = useCallback(RenderElement, []);
   return (
     <ActivePathContext.Provider value={{ path, setPath }}>
       <Slate editor={editor} value={value} onChange={setValue}>
         <div className="h-screen w-screen grid grid-cols-2 gap-6 p-6">
-          <Editable renderElement={renderElement} onClick={() => setPath([])} />
+          <Editable />
           <ActiveNode />
         </div>
       </Slate>
