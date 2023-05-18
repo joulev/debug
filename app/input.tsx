9b84c60fb217955eac63770716ed2700672fb873
Vu Van Dung <me@joulev.dev>
2023-05-18 15:10:52 +0000

    add example

---
+++ app/input.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Input({ query }: { query: string | null }) {
+  const router = useRouter();
+  const [value, setValue] = useState(query ?? "")
+  useEffect(() => {
+    const debounceTimeout = setTimeout(() => {
+      router.push(value === "" ? "/" : `/?q=${encodeURIComponent(value)}`);
+    }, 300);
+    return () => clearTimeout(debounceTimeout);
+  }, [value]);
+  return <input value={value} onChange={e => setValue(e.target.value)} placeholder="Search" />;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-  return <div>Hello world</div>;
+import Input from "./input";
+
+async function search(query: string) {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  return `You searched for ${query}`;
+}
+
+async function SearchResult({ query }: { query: string | null }) {
+  if (!query) return <div>No query provided</div>;
+  const result = await search(query);
+  return <div>{result}</div>;
+}
+
+export default async function Page({ searchParams }: { searchParams: { q: string | null }}) {
+  return (
+    <div>
+      <Input query={searchParams.q} />
+      {/* @ts-expect-error ts dumdum */}
+      <SearchResult query={searchParams.q} />
+    </div>
+  );
 }
