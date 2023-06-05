39c80934d8654cc5f35e60da4c76151cda5c85f5
Vu Van Dung <me@joulev.dev>
2023-06-05 12:56:53 +0000

    middleware working perfectly well

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextRequest } from 'next/server';
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)'
+  ]
+};
+
+export function middleware(req: NextRequest) {
+  console.log("A new request came", req.nextUrl.pathname);
+}
