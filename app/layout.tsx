d758b34e95e0cc8086683390d4abff7ee6274f04
Vu Van Dung <me@joulev.dev>
2023-06-17 09:04:43 +0700

    working normally :think:
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "./globals.css";
+
+config.autoAddCss = false;
+
 export default function RootLayout({
   children,
 }: {
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 export default function Page() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <p>
+        Hello world look at this little envelope{" "}
+        <FontAwesomeIcon icon={faEnvelope} />
+      </p>
+      <p className="red">And this text should be red</p>
+    </div>
+  );
 }
