90a30c8a4f5278e79f7b1918ad4309f0c9f8bb0b
Vu Van Dung <me@joulev.dev>
2023-06-21 21:20:59 +0700

    trigger suspense manually with key
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Suspense } from "react";
+import Loading from "./loading";
+
 async function search(query: string | null) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   if (!query) return "No query provided";
   return `You searched for ${query}`;
+}
+
+async function Search({ query }: { query: string | null }) {
+  const result = await search(query);
+  return <div>{result}</div>;
 }
 
 export default async function Page({
@@ -9,6 +17,9 @@ export default async function Page({
 }: {
   searchParams: { q: string | null };
 }) {
-  const result = await search(searchParams.q);
-  return <div>{result}</div>;
+  return (
+    <Suspense fallback={<Loading />} key={searchParams.q || ""}>
+      <Search query={searchParams.q} />
+    </Suspense>
+  );
 }
