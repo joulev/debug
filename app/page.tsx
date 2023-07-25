379a970cd4bb9dbb2d2741ee66f4a0c9b1666e74
Vu Van Dung <me@joulev.dev>
2023-07-25 10:07:15 +0700

    add repro
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import {
+  Arrow,
+  Content,
+  Portal,
+  Root,
+  Trigger,
+} from "@radix-ui/react-dropdown-menu";
+
 export default function Page() {
-  return <div>Hello world</div>;
+  return (
+    <Root>
+      <Trigger>Click me</Trigger>
+      <Portal>
+        <Content>
+          <div>Hello world</div>
+          <Arrow />
+        </Content>
+      </Portal>
+    </Root>
+  );
 }
