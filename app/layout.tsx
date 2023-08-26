247a2b77a7dd8bdb1649bafb61979cac9b919473
Vu Van Dung <me@joulev.dev>
2023-08-26 22:26:50 +0700

    commit 1
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+// import { VercelToolbar } from "@vercel/toolbar/next";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+        {/* <VercelToolbar /> */}
+      </body>
     </html>
   );
 }
