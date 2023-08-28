2c5ec0df29a616d0b19bf1e1c80c2203a9558340
Vu Van Dung <me@joulev.dev>
2023-08-28 08:15:09 +0700

    copy code as-is
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/route.ts
+++ app/route.ts
@@ -1,10 +1,10 @@
-import fs from "fs/promises";
+import { writeFileSync } from "fs";
 
 export const dynamic = "force-dynamic";
 
 export async function GET(): Promise<Response> {
   try {
-    await fs.writeFile("/tmp/hello.txt", "hello world");
+    writeFileSync("/tmp/helloworld.txt", "hello world", "utf8");
     return new Response("Ok!");
   } catch (e) {
     return new Response(String(e), { status: 500 });
