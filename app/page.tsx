876d48aea3ac2aa43b6368ce7916dfbb76e5aae2
Vu Van Dung <me@joulev.dev>
2023-09-21 13:23:10 +0700

    add reproduction
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
-  return <div>Hello world</div>;
+export default async function Page() {
+  const value = await fetch("https://generate-secret.vercel.app/16").then((v) =>
+    v.text()
+  );
+  console.log(value);
+  return <div>{value}</div>;
 }
