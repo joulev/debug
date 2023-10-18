aa736f90f3e739eb496b148b3f6d3491f3eb673e
Vu Van Dung <me@joulev.dev>
2023-10-18 14:40:58 +0700

    add reproduction
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+import Markdown from "react-markdown";
+import rehypeShikiji, { RehypeShikijiOptions } from "rehype-shikiji";
+
+const md = `
+This is a code block
+
+\`\`\`ts
+export function add(a: number, b: number) {
+  return a + b;
+}
+\`\`\`
+`;
+
 export default function Page() {
-  return <div>Hello world</div>;
+  return (
+    <Markdown
+      rehypePlugins={[
+        [rehypeShikiji, { theme: "nord" } satisfies RehypeShikijiOptions],
+      ]}
+    >
+      {md}
+    </Markdown>
+  );
 }
