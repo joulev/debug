e89755b311f736928c8b6683a347f3752c106373
Vu Van Dung <me@joulev.dev>
2024-01-11 12:35:48 +0800

    add example
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,13 @@
+import "./globals.css";
+
+import localFont from "next/font/local";
+
+const inter = localFont({
+  src: "./InterVariable.woff2",
+  display: "swap",
+  variable: "--inter",
+});
+
 export default function RootLayout({
   children,
 }: {
@@ -6,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body className={`${inter.variable} font-sans text-slate-700`}>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
 export default function Page() {
-  return <div>Hello world</div>;
+  return (
+    <dl className="flex flex-col gap-4 p-4 [&_dt]:text-slate-500 [&_dd]:text-2xl">
+      <div>
+        <dt>Inter as on Tailwind websites</dt>
+        <dd contentEditable suppressContentEditableWarning>
+          The quick brown fox jumps over the lazy dog 0123456789
+        </dd>
+      </div>
+      <div>
+        <dt>Inter</dt>
+        <dd
+          style={{ fontFeatureSettings: "normal" }}
+          contentEditable
+          suppressContentEditableWarning
+        >
+          The quick brown fox jumps over the lazy dog 0123456789
+        </dd>
+      </div>
+    </dl>
+  );
 }
