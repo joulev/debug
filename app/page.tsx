53e65b3ccc6bdf6e40b56e6ce66b69cd0f77d5bb
Vu Van Dung <me@joulev.dev>
2024-01-11 15:23:39 +0800

    more example
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <dl className="flex flex-col gap-4 p-4 [&_dt]:text-slate-500 [&_dd]:text-2xl">
       <div>
-        <dt>Inter as on Tailwind websites</dt>
+        <dt>Inter as on Tailwind websites (cv02, cv03, cv04 and cv11)</dt>
         <dd contentEditable suppressContentEditableWarning>
           The quick brown fox jumps over the lazy dog 0123456789
         </dd>
@@ -11,6 +11,25 @@ export default function Page() {
         <dt>Inter</dt>
         <dd
           style={{ fontFeatureSettings: "normal" }}
+          contentEditable
+          suppressContentEditableWarning
+        >
+          The quick brown fox jumps over the lazy dog 0123456789
+        </dd>
+      </div>
+      <div>
+        <dt>
+          You can customise the{" "}
+          <a
+            href="https://rsms.me/inter/#features"
+            className="underline underline-offset-4"
+          >
+            font variant
+          </a>{" "}
+          as you please. Example for ss01, ss02 and cv11:
+        </dt>
+        <dd
+          style={{ fontFeatureSettings: "'ss01' 'ss02' 'cv11'" }}
           contentEditable
           suppressContentEditableWarning
         >
