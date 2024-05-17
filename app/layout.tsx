5bb5004115639ad35634e8925891553f819693ae
Vũ Văn Dũng <me@joulev.dev>
2024-05-17 06:16:59 +0000

    add metadataBase
    Signed-off-by: GitHub <noreply@github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,3 +10,9 @@ export default function RootLayout({
     </html>
   );
 }
+
+export const metadata = {
+  metadataBase: new URL(
+    "https://debug-git-nextjs-og-image-refresh-on-vercel-joulev-proj.vercel.app"
+  ),
+};
