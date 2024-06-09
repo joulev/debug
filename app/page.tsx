9918ac4d5e092b64055967f03dc2ff5f298c048c
Vũ Văn Dũng <me@joulev.dev>
2024-06-09 08:54:29 +0000

    add test app
    Signed-off-by: GitHub <noreply@github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
+
 export default function Page() {
-  return <div>Hello world</div>;
+  const token = cookies().get("token");
+  async function setToken() {
+    "use server";
+    const token = new Array(36)
+      .fill(0)
+      .map(() => Math.random().toString(36)[2])
+      .join("");
+    cookies().set("token", token);
+    revalidatePath("/");
+  }
+  return (
+    <div>
+      <div>{token ? token.value : "No token"}</div>
+      <form action={setToken}>
+        <button type="submit">Set token</button>
+      </form>
+    </div>
+  );
 }
